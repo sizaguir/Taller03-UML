33e9d8960ed9e28000bb2b2ed9098c6059e357b6
sizaguir <138946181+sizaguir@users.noreply.github.com>
2024-10-24 20:29:36 -0500

    Update Foro.java

---
+++ Foro.java
@@ -0,0 +1,33 @@
+package com.mycompany.ds;
+
+public class Foro {
+    private String id;
+    private String tematica;
+    private int numeroDeComentarios;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTematica() {
+        return tematica;
+    }
+
+    public void setTematica(String tematica) {
+        this.tematica = tematica;
+    }
+
+    public int getNumeroDeComentarios() {
+        return numeroDeComentarios;
+    }
+
+    public void setNumeroDeComentarios(int numeroDeComentarios) {
+        this.numeroDeComentarios = numeroDeComentarios;
+    }
+    
+    
+}
