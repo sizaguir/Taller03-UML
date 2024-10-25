090d2da73316746e4bd0852f55fec63931fe242b
sizaguir <138946181+sizaguir@users.noreply.github.com>
2024-10-24 20:28:40 -0500

    Update Estudiante.java

---
+++ Estudiante.java
@@ -0,0 +1,12 @@
+package com.mycompany.ds;
+
+public class Estudiante extends PersonalAcademico{
+        
+    public void solicitarInscripcionCurso(Curso curso) {
+        
+    }
+
+    public void realizarActividadSumativa(Curso curso, ActividadSumativa actividadSumativa) {
+        
+    }
+}
