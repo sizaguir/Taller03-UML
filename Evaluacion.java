d9cf1c4b1f80b93371c4c351f643a5f75491ab63
sizaguir <138946181+sizaguir@users.noreply.github.com>
2024-10-24 20:28:59 -0500

    Update Evaluacion.java

---
+++ Evaluacion.java
@@ -0,0 +1,15 @@
+package com.mycompany.ds;
+
+public class Evaluacion extends ActividadSumativa{
+    private int limiteDeTiempo;
+
+    public int getLimiteDeTiempo() {
+        return limiteDeTiempo;
+    }
+
+    public void setLimiteDeTiempo(int limiteDeTiempo) {
+        this.limiteDeTiempo = limiteDeTiempo;
+    }
+    
+    
+}
