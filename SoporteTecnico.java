b23300cd574a601ab808c840b4300af5e4b9a142
sizaguir <138946181+sizaguir@users.noreply.github.com>
2024-10-24 20:34:24 -0500

    Update SoporteTecnico.java

---
+++ SoporteTecnico.java
@@ -0,0 +1,16 @@
+package com.mycompany.ds;
+
+public class SoporteTecnico extends Usuario{
+    
+    public void brindarAsistencia(Usuario usuario) {
+        
+    }
+
+    public void resolverProblemaTecnico(Usuario usuario) {
+        
+    }
+
+    public void derivarProblemaDeGestion(Administrador administrador) {
+        
+    }
+}
