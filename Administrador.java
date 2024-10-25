3d8bfe3e2a6e56dfde21d9f630aaf3e471e187b6
sizaguir <138946181+sizaguir@users.noreply.github.com>
2024-10-24 20:27:49 -0500

    Update Administrador.java

---
+++ Administrador.java
@@ -0,0 +1,16 @@
+package com.mycompany.ds;
+
+public class Administrador extends Usuario{
+    public void crearUsuarioconRol(String usuario, String contrasena){
+        
+    }
+    public void asignarPermiso(String usuario){
+        
+    }
+    public void actualizarSoftware(){
+        
+    }
+    public void asignarResponsable(Curso Curso, Profesor responsable){
+        
+    }
+}
